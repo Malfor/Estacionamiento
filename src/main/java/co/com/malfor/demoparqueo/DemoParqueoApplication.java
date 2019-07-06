caf86883ce5addee5b2426f13e27946e6756e018
Rodolfo Barrios <rodolfo.9408@gmail.com>
2019-07-06 11:50:37 -0500

    Repo nuevo de prueba

---
+++ src/main/java/co/com/malfor/demoparqueo/DemoParqueoApplication.java
@@ -0,0 +1,13 @@
+package co.com.malfor.demoparqueo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoParqueoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoParqueoApplication.class, args);
+	}
+
+}
